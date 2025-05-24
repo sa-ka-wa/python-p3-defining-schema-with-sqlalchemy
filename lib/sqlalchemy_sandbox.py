ad5890afa314a3915dc5eec121c8346ad94761bb
sa-ka-wa <samiekaranjawainaina@gmail.com>
2025-05-24 09:07:57 +0300

    added student.db from alchemy

--- lib/sqlalchemy_sandbox.py
+++ lib/sqlalchemy_sandbox.py
@@ -6,7 +6,13 @@ from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
 class Student(Base):
+    __tablename__ = 'students'
+    id = Column(Integer, primary_key=True)
+    name = Column(String())
     pass
 
 if __name__ == '__main__':
+    engine = create_engine('sqlite:///student.db')
+    Base.metadata.create_all(engine)
+
     pass
